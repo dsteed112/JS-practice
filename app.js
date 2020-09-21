6adde433be366edbac32675e545aa9b3aa8945da
Daniel Steed <dsteed112@gmail.com>
2020-09-20 19:55:56 -0600

    third problem solved

--- app.js
+++ app.js
@@ -3,14 +3,26 @@
 
 const isEqualTo100 = (a,b) => a === 100 || b === 100 || (a + b) === 100;
 
-console.log(isEqualTo100(100,0))
-console.log(isEqualTo100(50,50))
-console.log(isEqualTo100(43,22))
+console.log(isEqualTo100(100,0));
+console.log(isEqualTo100(50,50));
+console.log(isEqualTo100(43,22));
 
 
-//Write a JS progam to get the extension of a filename
+// Write a JS program to get the extension of a filename
 
 const getFileExtension = (string) => string.slice(string.lastIndexOf('.'))
 
-console.log(getFileExtension('index.html'))
-console.log(getFileExtension('webpack.config.js'))
+console.log(getFileExtension('index.html'));
+console.log(getFileExtension('webpack.config.js'));
+
+
+// Write a JS program to replace every character in a given string with 
+// the character following it in the alphabet
+
+const moveCharactersForward = (string) => 
+    string
+    .split('')
+    .map(char => String.fromCharCode(char.charCodeAt(0) + 1))
+    .join('');
+
+console.log(moveCharactersForward('abcd'));
